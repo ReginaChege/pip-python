5b0de31dc63e0853bbccbb14ed47059a6fe9703c
ReginaChege <reginachegewairimu@gmail.com>
2023-04-22 20:07:56 +0300

    sixth/bin/python3.10

--- python_plan.py
+++ python_plan.py
@@ -51,6 +51,75 @@ kitchen2={"desert","water","coster","fruits"}
 print(kitchen.difference_update(kitchen2))
 
 
+def odd():
+    n=1
+    sum=0
+    while(n>=100):
+        if(n%2==0):
+            print(f"{n} is divisible by 2")
+        else:
+            print(f"{n} is not divisible by 2")    
+odd()
+
+# Write a Python function that takes a list of integers as input and returns the product of all the
+#  numbers in the list
+def list_integer(numberss):
+    product=1
+    for n in numberss:
+        product*=n
+    return product
+numberss=[1,2,3,4,5,6,7,8,9,10]
+result = list_integer(numberss)
+print(result)
+
+ 
+# Write a Python program that takes a list of strings as input and returns
+#  the longest string in the list.
+def longString(names):
+    empty=""
+    for s in names:
+        if len(s)> len(empty):
+            empty=s
+    return empty
+
+names=['Regina','Gisemba','Annete']
+output=longString(names)
+print(output)
+        
+
+        
+            
+#  Write a Python function that takes a list of strings as input 
+# and returns a new list of the strings sorted alphabetically.
+def different(classes):
+     return sorted(classes)
+
+classes = ["Hopper","AnatiB","Ada"]
+stringSorted = different(classes)
+print(stringSorted)
+
+
+# Write a Python function that takes a string as input and returns a list
+#  of all the words in the string that have more than three letters.
+def long_words(string):
+    words=string.split
+    long_words=[]
+    for word in words:
+        if len(word)>3:
+            long_words.append(word)
+            return long_words
+
+String="The begining is always today"
+output=long_words(String)
+print(output)
+
+
+
+
+
+    
+    
+
 
 
 
